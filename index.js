e36b0526f0475244b73500b7adc465c75dfaca94
Adam <adamassalimy@gmail.com>
2022-10-22 13:02:08 -0700

    commit update

--- index.js
+++ index.js
@@ -1,55 +1,46 @@
-let homeTotal= document.getElementById("home-points")
-let guestTotal= document.getElementById("guest-points")
-let newgame = document.getElementById("home-points")
-let endgame =document.getElementById("guest-points")
+const homeTotal = document.getElementById("home-points");
+const guestTotal = document.getElementById("guest-points");
 
-let count = 0
+let countHome = 0;
+let countGuest = 0;
 
-function zerOhome(){
-    let zerOhome = count * 0
-    newgame.textContent = zerOhome
+function zeroHome() {
+  countHome = 0;
+  return (homeTotal.textContent = countHome);
 }
-function zerOguest(){
-    let zerOguest = count * 0
-    endgame.textContent = zerOguest
-}
+function zeroGuest() {
+  countGuest = 0;
 
-function addOnehome(){
-    let addOnehome = count + 1
-   homeTotal.textContent -= addOnehome 
-      
+  return (guestTotal.textContent = countGuest);
 }
 
+function addOneHome() {
+  countHome += 1;
 
-function addTwohome(){
-     let addTwohome = count + 2
-     homeTotal.textContent -=  addTwohome 
-      
+  return (homeTotal.textContent = countHome);
 }
 
-function addThreehome(){
-     let addThreehome = count + 3
-     homeTotal.textContent -=  addThreehome
-     
-     
+function addTwoHome() {
+  countHome += 2;
+  return (homeTotal.textContent = countHome);
 }
 
-function addOneguest(){
-    let addOneguest = count + 1
-    guestTotal.textContent -= addOneguest
-      
+function addThreeHome() {
+  countHome += 3;
+  return (homeTotal.textContent = countHome);
 }
 
-
-function addTwoguest(){
-     let addTwoguest = count + 2
-     guestTotal.textContent -= addTwoguest 
-    
+function addOneGuest() {
+  countGuest += 1;
+  return (guestTotal.textContent = countGuest);
 }
 
-function addThreeguest(){
-     let addThreeguest = count + 3
-     guestTotal.textContent -= addThreeguest
-     
+function addTwoGuest() {
+  countGuest += 2;
+  return (guestTotal.textContent = countGuest);
 }
 
+function addThreeGuest() {
+  countGuest += 3;
+  return (guestTotal.textContent = countGuest);
+}
